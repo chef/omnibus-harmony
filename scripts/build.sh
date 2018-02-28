2e90728f78aac5e7026620212f1a0e62a28d6b60
Scott Hain <shain@chef.io>
2018-02-28 12:24:51 -0800

    Test some buildkite
    Signed-off-by: Scott Hain <shain@chef.io>

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+export PROJECT_NAME=harmony
+
+bundle install --without development --path vendor/bundle --retry 5
+
+OMNIBUS_BUILD_OPTIONS="-l internal --populate-s3-cache --override s3_profile:omnibus-cache fatal_licensing_warnings:true cache_suffix:$PROJECT_NAME append_timestamp:false"
+
+bundle exec omnibus build $PROJECT_NAME $OMNIBUS_BUILD_OPTIONS
