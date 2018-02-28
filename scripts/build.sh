f30619435f3f9446065003f3a606dd616fc8890f
Scott Hain <shain@chef.io>
2018-02-28 13:04:32 -0800

    Make opt dir
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,10 @@ set -e
 
 export PROJECT_NAME=harmony
 
+sudo rm -rf /opt/$PROJECT_NAME
+sudo mkdir /opt/$PROJECT_NAME
+sudo chown build:build /opt/$PROJECT_NAME
+
 bundle install --without development --path vendor/bundle --retry 5
 
 OMNIBUS_BUILD_OPTIONS="-l internal --populate-s3-cache --override s3_profile:omnibus-cache fatal_licensing_warnings:true base_dir:~/omnibus cache_suffix:$PROJECT_NAME append_timestamp:false"
