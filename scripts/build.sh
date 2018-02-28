be75c20f8fd3af5b94aa9f1b86d316887a6db09b
Scott Hain <shain@chef.io>
2018-02-28 13:20:01 -0800

    Make cache dir - stop messing with overrides
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,12 @@ export PROJECT_NAME=harmony
 
 sudo rm -rf /opt/$PROJECT_NAME
 sudo mkdir /opt/$PROJECT_NAME
-sudo chown build /opt/$PROJECT_NAME
+sudo chown build:staff /opt/$PROJECT_NAME
+sudo mkdir -p /var/cache/omnibus
+sudo chmod 777 /var/cache/omnibus
 
 bundle install --without development --path vendor/bundle --retry 5
 
-OMNIBUS_BUILD_OPTIONS="-l internal --populate-s3-cache --override s3_profile:omnibus-cache fatal_licensing_warnings:true cache_dir:~/omnibus cache_suffix:$PROJECT_NAME append_timestamp:false"
+OMNIBUS_BUILD_OPTIONS="-l internal --populate-s3-cache --override s3_profile:omnibus-cache fatal_licensing_warnings:true cache_suffix:$PROJECT_NAME append_timestamp:false"
 
 bundle exec omnibus build $PROJECT_NAME $OMNIBUS_BUILD_OPTIONS
