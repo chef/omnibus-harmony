80fffbd26f0c529b101553fe6693855050580343
Scott Hain <shain@chef.io>
2018-03-12 12:51:12 -0700

    Try some more docker
    Signed-off-by: Scott Hain <shain@chef.io>

---
+++ scripts/build_ubuntu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+export PROJECT_NAME=harmony
+
+apt-get install -y autoconf binutils-doc bison build-essential flex gettext ncurses-dev sudo
+
+curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P omnibus-toolchain
+
+groupadd admin
+useradd -d /home/omnibus -U -s /bin/bash -m omnibus
+echo "omnibus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/omnibus
+chmod 0440 /etc/sudoers.d/omnibus
+
+rm -rf /opt/$PROJECT_NAME
+mkdir /opt/$PROJECT_NAME
+chown omnibus:admin /opt/$PROJECT_NAME
+mkdir -p /var/cache/omnibus
+chmod 777 /var/cache/omnibus
+
+sudo su - omnibus
+
+bundle install --without development --path vendor/bundle --retry 5
+
+OMNIBUS_BUILD_OPTIONS="-l internal --populate-s3-cache --override s3_profile:omnibus-cache fatal_licensing_warnings:true cache_suffix:$PROJECT_NAME append_timestamp:false"
+
+bundle exec omnibus build $PROJECT_NAME $OMNIBUS_BUILD_OPTIONS
