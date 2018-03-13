471aa0e9373effda5f911e8a6cd85ed89cd8c4e9
Scott Hain <shain@chef.io>
2018-03-13 14:42:18 -0700

    Add more pipelines
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/build_rhel.sh
+++ scripts/build_rhel.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export PROJECT_NAME=harmony
+# Build the project itself here
+bash -c /home/omnibus/build_project.sh
+
+# Upload the artifact to the buildkite job
+buildkite-agent artifact upload /home/omnibus/$PROJECT_NAME/pkg/*.rpm
