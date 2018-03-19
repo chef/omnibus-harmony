360f7f4c81a45578292fc90b962df53c8caef4e4
Scott Hain <shain@chef.io>
2018-03-19 12:25:03 -0700

    Am I inside or outside
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/test_rhel.sh
+++ scripts/test_rhel.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-env
-uname -a
-
-buildkite-agent artifact download "*.el7.*.rpm" .
+buildkite-agent artifact download "*.el7.*.rpm" tmp/
 
 # Build the project itself here
 bash -c /home/omnibus/test_project.sh
+
+ls -la /home/omnibus
