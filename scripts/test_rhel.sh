3bffc78d144ad21b3ad75f786a3f4a8932d4dde6
Scott Hain <shain@chef.io>
2018-03-19 13:08:34 -0700

    Remember rpm flags
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/test_rhel.sh
+++ scripts/test_rhel.sh
@@ -4,7 +4,7 @@ set -e
 sudo buildkite-agent artifact download "*.el7.*.rpm" .
 ls -la /opt/
 
-sudo rpm -i *.rpm -y
+sudo rpm -i *.rpm
 
 # Build the project itself here
 bash -c /home/omnibus/test_project.sh
