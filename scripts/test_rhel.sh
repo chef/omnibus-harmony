dddc1a4820b82cb1cd5db223254d8b66570574b3
Scott Hain <shain@chef.io>
2018-03-19 12:12:31 -0700

    Fix typo and test what we are
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/test_rhel.sh
+++ scripts/test_rhel.sh
@@ -2,11 +2,9 @@
 set -e
 
 env
+uname -a
 
-buildkit-agent artifact download "pkg/*.el7.rpm"
+buildkite-agent artifact download "pkg/*.el7.rpm"
 
 # Build the project itself here
-bash -c "$VERIFY_COMMAND"
-
-# # Upload the artifact to the buildkite job
-# buildkite-agent artifact upload /home/omnibus/$PROJECT_NAME/pkg/*.rpm
+bash -c /home/omnibus/test_project.sh
