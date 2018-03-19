d2ea4f08e86412a55847f45c63d1d5366488700f
Scott Hain <shain@chef.io>
2018-03-19 12:40:27 -0700

    The docker mount system is odd
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/test_rhel.sh
+++ scripts/test_rhel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-sudo buildkite-agent artifact download "*.el7.*.rpm" tmp/
+sudo buildkite-agent artifact download "*.el7.*.rpm" .
 
 # Build the project itself here
 bash -c /home/omnibus/test_project.sh
