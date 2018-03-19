309ef52b7469df3e2caf0bb4345c65bb42c8b574
Scott Hain <shain@chef.io>
2018-03-19 13:37:03 -0700

    bk makes all the directories with download
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/test_rhel.sh
+++ scripts/test_rhel.sh
@@ -3,9 +3,9 @@ set -e
 
 sudo buildkite-agent artifact download "*.el7.*.rpm" .
 ls -la /opt/
-ls -la .
+ls -la home/omnibus/harmony/pkg/
 
-sudo rpm -i *.rpm
+sudo rpm -i home/omnibus/harmony/pkg/*.rpm
 
 # Build the project itself here
 bash -c /home/omnibus/test_project.sh
