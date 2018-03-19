1b05f3cb443ebba3f369709c5786f7a11389f6e1
Scott Hain <shain@chef.io>
2018-03-19 16:10:28 -0700

    Fix order of args
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/build_rhel.sh
+++ scripts/build_rhel.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Build the project itself here
-export OMNIBUS_BUILD_OPTIONS="-l internal fatal_licensing_warnings:true cache_suffix:$PROJECT_NAME append_timestamp:false"
+export OMNIBUS_BUILD_OPTIONS="-l internal --override cache_suffix:$PROJECT_NAME fatal_licensing_warnings:true cache_suffix:$PROJECT_NAME append_timestamp:false"
 /home/omnibus/build_project.sh
 
 # Upload the artifact to the buildkite job
