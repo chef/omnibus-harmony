c85ab6b45c8a5fc548e0534616a0612586dd5c59
Tom Duffield <tom@chef.io>
2021-02-15 16:16:27 -0600

    Update MSI package config with latest details
    Signed-off-by: Tom Duffield <tom@chef.io>

--- config/projects/harmony.rb
+++ config/projects/harmony.rb
@@ -41,6 +41,8 @@ end
 compress :dmg
 
 package :msi do
-  upgrade_code '3AA89B1F-D8F3-4D46-8CB2-534C8313DBFD'
-  signing_identity "E05FF095D07F233B78EB322132BFF0F035E11B5B", machine_store: true
+  fast_msi true
+  git upgrade_code "3AA89B1F-D8F3-4D46-8CB2-534C8313DBFD"
+  wix_light_extension "WixUtilExtension"
+  signing_identity "AF21BA8C9E50AE20DA9907B6E2D4B0CC3306CA03", machine_store: true
 end
