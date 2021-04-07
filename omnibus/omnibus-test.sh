5cfaf4e19f41ac111cfd488cc8cc94ac8f1a0940
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-04-07 10:15:56 -0400

    Add logic to use package file location or pull from artifactory
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -4,9 +4,12 @@ set -ueo pipefail
 channel="${CHANNEL:-unstable}"
 product="${PRODUCT:-harmony}"
 version="${VERSION:-latest}"
+package_file=${PACKAGE_FILE:-""}
 
 echo "--- Installing $channel $product $version"
-package_file="$(/opt/omnibus-toolchain/bin/install-omnibus-product -c "$channel" -P "$product" -v "$version" | tail -n 1)"
+if [[ -z $package_file ]]; then
+  package_file="$(/opt/omnibus-toolchain/bin/install-omnibus-product -c "$channel" -P "$product" -v "$version" | tail -n 1)"
+fi
 
 echo "--- Verifying omnibus package is signed"
 /opt/omnibus-toolchain/bin/check-omnibus-package-signed "$package_file"
