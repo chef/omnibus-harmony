6852bfce994bd9ae9a46f3e261ac9caec3ff3ae1
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-04-15 09:34:47 -0400

    Update echo inside if
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -6,8 +6,8 @@ product="${PRODUCT:-harmony}"
 version="${VERSION:-latest}"
 package_file=${PACKAGE_FILE:-""}
 
-echo "PACKAGE FILE $package_file"
 if [[ -z $package_file ]]; then
+  echo "PACKAGE FILE $package_file"
   package_file="$(.omnibus-buildkite-plugin/install-omnibus-product.sh -c "$channel" -P "$product" -v "$version" | tail -n 1)"
 else
   .omnibus-buildkite-plugin/install-omnibus-product.sh -f "$package_file" -P "$product" -v "$version" &> /dev/null
