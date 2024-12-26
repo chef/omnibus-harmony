d249180bc0cc08475450d264ac905914cee0f903
sean-simmons-progress <111799713+sean-simmons-progress@users.noreply.github.com>
2024-12-26 14:55:37 -0500

    Sean simmons progress/habworkflowtesting2 (#95)
    * attempting to work with the proper channel to release rubygems, and also promote habitat packages
    
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>
    
    * adding changes into a new branch
    
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>
    
    * updating script
    
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>
    
    ---------
    
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

---
+++ .expeditor/scripts/promote-hab-pkgs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+# The following standard Expeditor artifact_actions environment variables
+# are automatically set by the calling process:
+#
+# PROMOTABLE - in promotion artifact actions this is a reference to the
+#              source channel.
+# TARGET_CHANNEL - the channel which we are promoting to
+# HAB_AUTH_TOKEN - GitHub Auth token used to communicate with the
+#                  Habitat depot and private repos in Chef's GitHub org
+#
+set -eou pipefail
+
+# Export the HAB_AUTH_TOKEN for use of promoting habitat packages to {{TARGET_CHANNE}}
+HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
+export HAB_AUTH_TOKEN
+
+if [ "${EXPEDITOR_TARGET_CHANNEL}" = "unstable" ];
+then
+  echo "This file does not support actions for artifacts promoted to unstable"
+  exit 1
+fi
+
+
+# Promote the artifacts in Habitat Depot
+jq -r -c ".packages[]" manifest.json | while read -r service_ident; do
+  pkg_origin=${service_ident%/*/*/*}
+
+  if [ "$pkg_origin" = "core" ];
+  then
+    echo "Skipping promotion of core origin package ${service_ident}"
+  else
+    echo "Promoting ${service_ident} to the ${EXPEDITOR_TARGET_CHANNEL} channel"
+    hab pkg promote "${service_ident}" "${EXPEDITOR_TARGET_CHANNEL}"
+  fi
+done
