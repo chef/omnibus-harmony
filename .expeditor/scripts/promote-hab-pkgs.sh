f5fdf2a9298e892063fe9977f5d04ece29b21e36
Sean Simmons <sean.simmons@progress.com>
2024-12-26 14:46:35 -0500

    updating script
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

--- .expeditor/scripts/promote-hab-pkgs.sh
+++ .expeditor/scripts/promote-hab-pkgs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Export the HAB_AUTH_TOKEN for use of promoting habitat packages to {{TARGET_CHANNE}}
-
-
 
 #
 # The following standard Expeditor artifact_actions environment variables
@@ -13,9 +10,11 @@
 # HAB_AUTH_TOKEN - GitHub Auth token used to communicate with the
 #                  Habitat depot and private repos in Chef's GitHub org
 #
-
-
 set -eou pipefail
+
+# Export the HAB_AUTH_TOKEN for use of promoting habitat packages to {{TARGET_CHANNE}}
+HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
+export HAB_AUTH_TOKEN
 
 if [ "${EXPEDITOR_TARGET_CHANNEL}" = "unstable" ];
 then
@@ -23,5 +22,16 @@ then
   exit 1
 fi
 
-HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
-export HAB_AUTH_TOKEN
+
+# Promote the artifacts in Habitat Depot
+jq -r -c ".packages[]" manifest.json | while read -r service_ident; do
+  pkg_origin=${service_ident%/*/*/*}
+
+  if [ "$pkg_origin" = "core" ];
+  then
+    echo "Skipping promotion of core origin package ${service_ident}"
+  else
+    echo "Promoting ${service_ident} to the ${EXPEDITOR_TARGET_CHANNEL} channel"
+    hab pkg promote "${service_ident}" "${EXPEDITOR_TARGET_CHANNEL}"
+  fi
+done
