5d802967500706856832b9487bff6302a1e917ef
Sonu Saha <sonu.saha@progress.com>
2025-01-27 17:50:39 +0530

    chore: minor text change to bump version
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/omnibus-harmony.rb
+++ lib/omnibus-harmony.rb
@@ -3,6 +3,6 @@ require "omnibus-harmony/version"
 module OmnibusHarmony
   class Error < StandardError; end
   def self.hello
-    puts "Hello from Sean Simmons! If you see this message it means that the include rubygem is working"
+    puts "Hello from Sean Simmons! If you see this message it means that the include rubygem is working."
   end
 end
