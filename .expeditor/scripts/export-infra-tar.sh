029f3a63512257ed636bf8cc706c2c0582c131cb
rishichawda <rishichawda@users.noreply.github.com>
2025-02-04 16:12:47 +0530

    test the whole flow
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/export-infra-tar.sh
+++ .expeditor/scripts/export-infra-tar.sh
@@ -2,29 +2,16 @@
 
 set -euo pipefail
 
-echo "fetching latest release info"
-curl -fSL -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/chef/migration-tools/releases/latest -o migration-tool-latest-release.json
-latest_version=$(cat migration-tool-latest-release.json | jq -r '.tag_name')
+echo "--- hab export tar chef/chef-infra-client"
+hab pkg export tar chef/chef-infra-client --channel unstable
 
-echo "requesting migration-tools_Linux_x86_64.tar.gz from $latest_version release"
-cat migration-tool-latest-release.json \
-    | jq '.assets[] | select (.name == "migration-tools_Linux_x86_64.tar.gz") | .url' \
-    | xargs curl -fSL -H "Authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/octet-stream" -o "migration-tools_Linux_x86_64.tar.gz"
+echo "--- upload tar to artifactory and s3"
+tar_name=$(find . -regex "./chef-chef-infra-client-[0-9]+\.[0-9]+\.[0-9]+-[0-9]+\.tar\.gz" -type f -exec basename {} \;) # tar name. pass this to next steps? 
+mkdir -p upload && cp $tar_name upload/
 
-file migration-tools_Linux_x86_64.tar.gz
+cd upload/
+buildkite-agent artifact upload $tar_name
+aws s3 cp $tar_name s3://chef-hab-migration-tool-bucket/rc2_hab_pkg_chef_client/rc2_tar_folder/$tar_name
 
-# curl -fSL -H "Authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/octet-stream" https://api.github.com/repos/chef/migration-tools/releases/assets/217686273 -o migration-tools_Linux_x86_64.tar.gz
-
-# echo "--- hab export tar chef/chef-infra-client"
-# hab pkg export tar chef/chef-infra-client --channel unstable
-
-# echo "--- upload tar to artifactory and s3"
-# tar_name=$(find . -regex "./chef-chef-infra-client-[0-9]+\.[0-9]+\.[0-9]+-[0-9]+\.tar\.gz" -type f -exec basename {} \;) # tar name. pass this to next steps? 
-# mkdir -p upload && cp $tar_name upload/
-
-# cd upload/
-# buildkite-agent artifact upload $tar_name
-# aws s3 cp $tar_name s3://chef-hab-migration-tool-bucket/rc2_hab_pkg_chef_client/rc2_tar_folder/$tar_name
-
-# echo "--- update meta-data PACKAGE_TAR_FILENAME to $tar_name"
-# buildkite-agent meta-data set "PACKAGE_TAR_FILENAME" $tar_name
+echo "--- update meta-data PACKAGE_TAR_FILENAME to $tar_name"
+buildkite-agent meta-data set "PACKAGE_TAR_FILENAME" $tar_name
--- .expeditor/scripts/package-rpm.sh
+++ .expeditor/scripts/package-rpm.sh
@@ -10,9 +10,7 @@ tar_name=$(buildkite-agent meta-data get "PACKAGE_TAR_FILENAME")
 echo "--- using PACKAGE_TAR_FILENAME=$tar_name from meta-data"
 
 echo "--- setting env variables CHEF_INFRA_MIGRATE_TAR and CHEF_INFRA_HAB_TAR"
-export CHEF_INFRA_MIGRATE_TAR=https://github.com/chef/migration-tools/releases/latest/download/migration-tools_Linux_x86_64.tar.gz
-# switch to github url once github access is fixed.
-# export CHEF_INFRA_MIGRATE_TAR=s3://chef-hab-migration-tool-bucket/rc2_hab_pkg_chef_client/rc2_migration_tool/migration-tools_Linux_x86_64.tar.gz
+export CHEF_INFRA_MIGRATE_TAR='migration-tools_Linux_x86_64.tar.gz'
 export CHEF_INFRA_HAB_TAR=$tar_name
 
 echo "--- running build script"
