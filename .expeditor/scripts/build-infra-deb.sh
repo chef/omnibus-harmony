696bda542740d73d7d6fe951f8714916294e0e97
muthuja <muthujabavaji.vempalli@progress.com>
2025-02-05 15:29:11 +0530

    print github token
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .expeditor/scripts/build-infra-deb.sh
+++ .expeditor/scripts/build-infra-deb.sh
@@ -44,7 +44,7 @@ download_migration_tool_from_github_releases() {
     local output_path="$1"
 
     echo "--- Downloading migration tools to $output_path.."
-    echi "-------DEBUG-------"
+    echo "-------DEBUG-------"
     echo "$GITHUB_TOKEN"
 
     if [ -z "${GITHUB_TOKEN:-}" ]; then
